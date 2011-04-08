911ccfdd6274ef306618baf1aa9cbeda27ec4400
John Britton <public@johndbritton.com>
2011-04-08 02:16:30 -0700

    removed unneeded decks.rb

--- lib/cardshark/decks.rb
+++
@@ -1,9 +0,0 @@
-module Cardshark
-  module Decks
-    FRENCH = {
-      :ranks => [:ace, :two, :three, :four, :five, :six, :seven, :eight, :nine, :ten, :jack, :queen, :king], 
-      :suits => [:spades, :hearts, :diamonds, :clubs],
-      :special => []
-    }
-  end
-end
