ecd300e74d9e57af23bda18d84adacf5098d9e9d
John Britton <public@johndbritton.com>
2011-04-08 02:16:55 -0700

    added tests for FrenchDeck

--- spec/french_deck_spec.rb
+++ spec/french_deck_spec.rb
@@ -4,9 +4,14 @@ describe Cardshark::FrenchDeck do
   #create a french (standard) deck
   before :all do
     @french = Cardshark::FrenchDeck.new
+    @french_with_jokers = Cardshark::FrenchDeck.new(:include_jokers => true)
   end
 
   it 'should contain 52 cards' do
     @french.size.should == 52
+  end
+  
+  it 'should contain 54 cards' do
+    @french_with_jokers.size == 54
   end
 end
