71c6abae1d69768673130a60c3fde3a4a1286cb7
John Britton <public@johndbritton.com>
2011-04-08 03:41:39 -0700

    added shuffling and related tests

--- lib/cardshark/deck.rb
+++ lib/cardshark/deck.rb
@@ -7,5 +7,15 @@ module Cardshark
     def cards
       @cards
     end
+    
+    def shuffle!
+      @cards.shuffle!
+      @shuffled = true
+    end
+    
+    def shuffled?
+      return false unless @shuffled
+      true
+    end
   end
 end
