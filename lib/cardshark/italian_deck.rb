578fc02ea6aaac2f71316d8c45040c088ddada3a
Caley Woods <caley.w@gmail.com>
2011-04-29 15:02:10 -0500

    No longer need attr_reader

--- lib/cardshark/italian_deck.rb
+++ lib/cardshark/italian_deck.rb
@@ -5,8 +5,10 @@ module Cardshark
   # Representation of the 40 Card italian deck. http://en.wikipedia.org/wiki/Playing_card#Italian
 
   class ItalianDeck
-    attr_reader :SUITS
     include Deck
+    # The wiki article states the face cards are Re(king), Cavallo(queen),
+    # and Fante(jack) but i've kept them the same as a French deck here
+    # for consistency sake
     RANKS = [:ace, :two, :three, :four, :five, :six, :seven, :jack, :queen, :king]
     SUITS = [:coins, :swords, :cups, :clubs]
 
