7f872af53cf6c9de8ad37fd2c63b10330a59a9ae
John Britton <public@johndbritton.com>
2020-02-02 00:24:29 -0500

    remove tests and implementations

--- lib/cardshark/card.rb
+++ lib/cardshark/card.rb
@@ -1,10 +1,2 @@
-module Cardshark
-  class Card
-    attr_reader :rank, :suit
-    def initialize(card)
-      @rank = card[:rank]
-      @suit = card[:suit]
-      self.freeze
-    end
-  end
+class Cardshark::Card
 end
--- spec/cardshark/card_spec.rb
+++ spec/cardshark/card_spec.rb
@@ -1,15 +1,4 @@
 require 'cardshark/card'
 
 describe Cardshark::Card do
-  before :all do
-    @card = Cardshark::Card.new(:rank => :K, :suit => :H)
-  end
-  
-  it 'has a rank' do
-    @card.rank.should == :K
-  end
-  
-  it 'has a suit' do
-    @card.suit.should == :H
-  end
 end
