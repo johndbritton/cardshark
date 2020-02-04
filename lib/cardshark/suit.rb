babee518d8d16b33e295add2f83d7c664612eed5
John Britton <public@johndbritton.com>
2020-02-03 23:17:11 -0500

    add base Suit class as a Dimension

---
+++ lib/cardshark/suit.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'cardshark/error'
+require 'cardshark/dimension'
+
+module Cardshark
+  class Suit
+    include Dimension
+  end
+end
---
+++ spec/cardshark/suit_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'cardshark/rank'
+
+RSpec.describe Cardshark::Rank do
+end
