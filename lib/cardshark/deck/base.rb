e6bfa255be936c389edf36b5d096283085688b7d
John Britton <public@johndbritton.com>
2020-02-04 22:21:23 -0500

    make Deck::Base abstract

--- lib/cardshark/deck/base.rb
+++ lib/cardshark/deck/base.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
+require 'cardshark/error'
 require 'cardshark/card'
 
 module Cardshark
   module Deck
     class Base
+      @abstract = true
+
+      def self.abstract?
+        if @abstract
+          true
+        else
+          false
+        end
+      end
+
+      def self.inherited(subclass)
+        subclass.instance_variable_set(:@abstract, false)
+      end
+
       def initialize(cards)
+        raise Error::AbstractClass if self.class.abstract?
+
         @cards = cards
         raise ArgumentError unless valid_cards?
       end
--- spec/cardshark/deck/base_spec.rb
+++ spec/cardshark/deck/base_spec.rb
@@ -26,111 +26,124 @@ RSpec.describe Cardshark::Deck::Base do
   let(:queen)    { Class.new(rank).new }
   let(:king)     { Class.new(rank).new }
 
-  let(:ace_of_clubs)      { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:two_of_clubs)      { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:three_of_clubs)    { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:four_of_clubs)     { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:five_of_clubs)     { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:six_of_clubs)      { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:seven_of_clubs)    { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:eight_of_clubs)    { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:nine_of_clubs)     { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:ten_of_clubs)      { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:jack_of_clubs)     { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:queen_of_clubs)    { Cardshark::Card::Base.new([ace, clubs]) }
-  let(:king_of_clubs)     { Cardshark::Card::Base.new([ace, clubs]) }
+  let(:card_class)        { Class.new(Cardshark::Card::Base) }
+  let(:ace_of_clubs)      { card_class.new([ace, clubs]) }
+  let(:two_of_clubs)      { card_class.new([ace, clubs]) }
+  let(:three_of_clubs)    { card_class.new([ace, clubs]) }
+  let(:four_of_clubs)     { card_class.new([ace, clubs]) }
+  let(:five_of_clubs)     { card_class.new([ace, clubs]) }
+  let(:six_of_clubs)      { card_class.new([ace, clubs]) }
+  let(:seven_of_clubs)    { card_class.new([ace, clubs]) }
+  let(:eight_of_clubs)    { card_class.new([ace, clubs]) }
+  let(:nine_of_clubs)     { card_class.new([ace, clubs]) }
+  let(:ten_of_clubs)      { card_class.new([ace, clubs]) }
+  let(:jack_of_clubs)     { card_class.new([ace, clubs]) }
+  let(:queen_of_clubs)    { card_class.new([ace, clubs]) }
+  let(:king_of_clubs)     { card_class.new([ace, clubs]) }
 
-  let(:ace_of_diamonds)   { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:two_of_diamonds)   { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:three_of_diamonds) { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:four_of_diamonds)  { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:five_of_diamonds)  { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:six_of_diamonds)   { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:seven_of_diamonds) { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:eight_of_diamonds) { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:nine_of_diamonds)  { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:ten_of_diamonds)   { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:jack_of_diamonds)  { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:queen_of_diamonds) { Cardshark::Card::Base.new([ace, diamonds]) }
-  let(:king_of_diamonds)  { Cardshark::Card::Base.new([ace, diamonds]) }
+  let(:ace_of_diamonds)   { card_class.new([ace, diamonds]) }
+  let(:two_of_diamonds)   { card_class.new([ace, diamonds]) }
+  let(:three_of_diamonds) { card_class.new([ace, diamonds]) }
+  let(:four_of_diamonds)  { card_class.new([ace, diamonds]) }
+  let(:five_of_diamonds)  { card_class.new([ace, diamonds]) }
+  let(:six_of_diamonds)   { card_class.new([ace, diamonds]) }
+  let(:seven_of_diamonds) { card_class.new([ace, diamonds]) }
+  let(:eight_of_diamonds) { card_class.new([ace, diamonds]) }
+  let(:nine_of_diamonds)  { card_class.new([ace, diamonds]) }
+  let(:ten_of_diamonds)   { card_class.new([ace, diamonds]) }
+  let(:jack_of_diamonds)  { card_class.new([ace, diamonds]) }
+  let(:queen_of_diamonds) { card_class.new([ace, diamonds]) }
+  let(:king_of_diamonds)  { card_class.new([ace, diamonds]) }
 
-  let(:ace_of_hearts)     { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:two_of_hearts)     { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:three_of_hearts)   { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:four_of_hearts)    { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:five_of_hearts)    { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:six_of_hearts)     { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:seven_of_hearts)   { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:eight_of_hearts)   { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:nine_of_hearts)    { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:ten_of_hearts)     { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:jack_of_hearts)    { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:queen_of_hearts)   { Cardshark::Card::Base.new([ace, hearts]) }
-  let(:king_of_hearts)    { Cardshark::Card::Base.new([ace, hearts]) }
+  let(:ace_of_hearts)     { card_class.new([ace, hearts]) }
+  let(:two_of_hearts)     { card_class.new([ace, hearts]) }
+  let(:three_of_hearts)   { card_class.new([ace, hearts]) }
+  let(:four_of_hearts)    { card_class.new([ace, hearts]) }
+  let(:five_of_hearts)    { card_class.new([ace, hearts]) }
+  let(:six_of_hearts)     { card_class.new([ace, hearts]) }
+  let(:seven_of_hearts)   { card_class.new([ace, hearts]) }
+  let(:eight_of_hearts)   { card_class.new([ace, hearts]) }
+  let(:nine_of_hearts)    { card_class.new([ace, hearts]) }
+  let(:ten_of_hearts)     { card_class.new([ace, hearts]) }
+  let(:jack_of_hearts)    { card_class.new([ace, hearts]) }
+  let(:queen_of_hearts)   { card_class.new([ace, hearts]) }
+  let(:king_of_hearts)    { card_class.new([ace, hearts]) }
 
-  let(:ace_of_spades)     { Cardshark::Card::Base.new([ace, spades]) }
-  let(:two_of_spades)     { Cardshark::Card::Base.new([ace, spades]) }
-  let(:three_of_spades)   { Cardshark::Card::Base.new([ace, spades]) }
-  let(:four_of_spades)    { Cardshark::Card::Base.new([ace, spades]) }
-  let(:five_of_spades)    { Cardshark::Card::Base.new([ace, spades]) }
-  let(:six_of_spades)     { Cardshark::Card::Base.new([ace, spades]) }
-  let(:seven_of_spades)   { Cardshark::Card::Base.new([ace, spades]) }
-  let(:eight_of_spades)   { Cardshark::Card::Base.new([ace, spades]) }
-  let(:nine_of_spades)    { Cardshark::Card::Base.new([ace, spades]) }
-  let(:ten_of_spades)     { Cardshark::Card::Base.new([ace, spades]) }
-  let(:jack_of_spades)    { Cardshark::Card::Base.new([ace, spades]) }
-  let(:queen_of_spades)   { Cardshark::Card::Base.new([ace, spades]) }
-  let(:king_of_spades)    { Cardshark::Card::Base.new([ace, spades]) }
+  let(:ace_of_spades)     { card_class.new([ace, spades]) }
+  let(:two_of_spades)     { card_class.new([ace, spades]) }
+  let(:three_of_spades)   { card_class.new([ace, spades]) }
+  let(:four_of_spades)    { card_class.new([ace, spades]) }
+  let(:five_of_spades)    { card_class.new([ace, spades]) }
+  let(:six_of_spades)     { card_class.new([ace, spades]) }
+  let(:seven_of_spades)   { card_class.new([ace, spades]) }
+  let(:eight_of_spades)   { card_class.new([ace, spades]) }
+  let(:nine_of_spades)    { card_class.new([ace, spades]) }
+  let(:ten_of_spades)     { card_class.new([ace, spades]) }
+  let(:jack_of_spades)    { card_class.new([ace, spades]) }
+  let(:queen_of_spades)   { card_class.new([ace, spades]) }
+  let(:king_of_spades)    { card_class.new([ace, spades]) }
 
   describe '::new' do
-    context 'invalid arguments' do
-      context 'missing arguments' do
-        it 'raises an ArgumentError' do
-          expect { described_class.new }.to raise_exception(ArgumentError)
-        end
-      end
-
-      context 'incorrect type' do
-        it 'raises an ArgumentError' do
-          expect { described_class.new('invalid arguments') }
-            .to raise_exception(ArgumentError)
-        end
-      end
-
-      context 'empty array' do
-        it 'raises an ArgumentError' do
-          expect { described_class.new([]) }.to raise_exception(ArgumentError)
-        end
-      end
-
-      context 'array with incorrect values' do
-        it 'raises an ArgumentError' do
-          expect { described_class.new(%w[incorrect values]) }
-            .to raise_exception(ArgumentError)
-        end
+    context 'valid arguments' do
+      let(:cards) { [ace_of_spades] }
+      it 'raises a Cardshark::Error::AbstractClass' do
+        expect { described_class.new(cards) }
+          .to raise_error(Cardshark::Error::AbstractClass)
       end
     end
 
-    context 'valid arguments' do
-      context 'one card' do
-        let(:cards) { [ace_of_spades] }
+    context 'when subclassed' do
+      let(:subclass) { Class.new(described_class) }
 
-        it 'returns a Deck' do
-          expect(described_class.new(cards)).to be_a(described_class)
+      context 'invalid arguments' do
+        context 'missing arguments' do
+          it 'raises an ArgumentError' do
+            expect { subclass.new }.to raise_exception(ArgumentError)
+          end
+        end
+
+        context 'incorrect type' do
+          it 'raises an ArgumentError' do
+            expect { subclass.new('invalid arguments') }
+              .to raise_exception(ArgumentError)
+          end
+        end
+
+        context 'empty array' do
+          it 'raises an ArgumentError' do
+            expect { subclass.new([]) }.to raise_exception(ArgumentError)
+          end
+        end
+
+        context 'array with incorrect values' do
+          it 'raises an ArgumentError' do
+            expect { subclass.new(%w[incorrect values]) }
+              .to raise_exception(ArgumentError)
+          end
         end
       end
 
-      context 'multiple cards' do
-        let(:cards) do
-          [ace_of_clubs, ace_of_diamonds, ace_of_hearts, ace_of_spades]
+      context 'valid arguments' do
+        context 'one card' do
+          let(:cards) { [ace_of_spades] }
+
+          it 'returns a Deck' do
+            expect(subclass.new(cards)).to be_a(subclass)
+          end
         end
 
-        before do
-          @result = described_class.new(cards)
-        end
+        context 'multiple cards' do
+          let(:cards) do
+            [ace_of_clubs, ace_of_diamonds, ace_of_hearts, ace_of_spades]
+          end
 
-        it 'returns a Deck' do
-          expect(@result).to be_a(described_class)
+          before do
+            @result = subclass.new(cards)
+          end
+
+          it 'returns a Deck' do
+            expect(@result).to be_a(subclass)
+          end
         end
       end
     end
